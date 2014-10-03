c454dbd80368c9ef0ce006f16774a98b421755e6
Clinton Andrews <candrews800@gmail.com>
2014-10-02 19:50:19 -0500

    update

--- app/controllers/SearchController.php
+++ app/controllers/SearchController.php
@@ -6,9 +6,12 @@ class SearchController extends BaseController {
         $search_tag = Tag::where('name', 'LIKE', '%'.$search_text.'%')->first();
 
         if($search_tag){
-            $recipes = Recipe::where('name', 'LIKE', '%'.$search_text.'%')
-                ->where('private', '!=', 't')
-                ->orWhere('related_tags', 'LIKE', '%'.$search_tag->id.'%')
+            $recipes = Recipe::where('private', '!=', 't')
+                ->where(function($query) use ($search_text, $search_tag)
+                {
+                    $query->where('name', 'LIKE', '%'.$search_text.'%')
+                          ->orWhere('related_tags', 'LIKE', '%'.$search_tag->id.'%');
+                })
                 ->get();
         }
         else{
