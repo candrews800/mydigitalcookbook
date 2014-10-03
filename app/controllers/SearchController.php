7d0e35cdae42599ed65cc4f828109ef48d32e351
Clinton Andrews <candrews800@gmail.com>
2014-10-02 19:46:39 -0500

    update

--- app/controllers/SearchController.php
+++ app/controllers/SearchController.php
@@ -8,7 +8,7 @@ class SearchController extends BaseController {
         if($search_tag){
             $recipes = Recipe::where('name', 'LIKE', '%'.$search_text.'%')
                 ->where('private', '!=', 't')
-                ->where('related_tags', 'LIKE', '%'.$search_tag->id.'%')
+                ->orWhere('related_tags', 'LIKE', '%'.$search_tag->id.'%')
                 ->get();
         }
         else{
